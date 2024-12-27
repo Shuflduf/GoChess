7a5a5bb281b4fdaaafb3f5b8a2fa38f4cd94e671
Shuflduf <shuflduf@gmail.com>
2024-12-26 19:08:18 -0700

    gonna start refactor

--- pieces.go
+++ pieces.go
@@ -13,6 +13,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Piece struct {
+	pieceType int
+	pos       [2]int
+	symbol    rune
+}
+
 //go:embed assets/pieces_atlas_big.png
 var textureData []byte
 
@@ -29,15 +35,15 @@ var pieceMap = map[rune]int{
 
 func GetPieceFromFen(r rune) *ebiten.Image {
 	index := pieceMap[unicode.ToLower(r)]
-  sign := unicode.IsLower(r)
-  if sign {
-    index *= -1
-  }
-  return GetPieceFromIndex(index)
+	sign := unicode.IsLower(r)
+	if sign {
+		index *= -1
+	}
+	return GetPieceFromIndex(index)
 }
 
 func GetPieceFromIndex(i int) *ebiten.Image {
-  absI := int(math.Abs(float64(i)))
+	absI := int(math.Abs(float64(i)))
 	cropRect := image.Rect((absI-1)*pieceSize, 0, absI*pieceSize, pieceSize)
 	if i < 0 {
 		cropRect = cropRect.Add(image.Point{0, pieceSize})
@@ -56,27 +62,27 @@ func LoadTexture() {
 }
 
 func ValidPositions(piece int, from [2]int) (valid [][2]int) {
-  switch int(math.Abs(float64(piece))) {
+	switch int(math.Abs(float64(piece))) {
 
-  //Pawn
-  case 6:
-    if piece > 0 {
-      t := from
-      t[1]--
-      valid = append(valid, t)
-      if from[1] == 6 {
-        t[1]--
-        valid = append(valid, t)
-      }
-    } else {
-      t := from
-      t[1]++
-      valid = append(valid, t)
-      if from[1] == 1 {
-        t[1]++
-        valid = append(valid, t)
-      }
-    }
-  }
-  return
+	//Pawn
+	case 6:
+		if piece > 0 {
+			t := from
+			t[1]--
+			valid = append(valid, t)
+			if from[1] == 6 {
+				t[1]--
+				valid = append(valid, t)
+			}
+		} else {
+			t := from
+			t[1]++
+			valid = append(valid, t)
+			if from[1] == 1 {
+				t[1]++
+				valid = append(valid, t)
+			}
+		}
+	}
+	return
 }
